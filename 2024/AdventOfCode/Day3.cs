66d9151f1aad9f7b76560573cb96ec51ddf8522d
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2024-12-08 23:00:42 +0100

    Created wrapper project

---
+++ 2024/AdventOfCode/Day3.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+using Common;
+
+namespace AdventOfCode;
+
+public static class Day3
+{
+    public static void Run()
+    {
+
+        var instructions = File.OpenText(@"..\..\..\inputs\day3.txt")
+            .ReadLines()
+            .SelectMany(x => x.MulCalculator())
+            .Sum();
+
+        Console.WriteLine("Sum of mul: {0}", instructions);
+    }
+
+    private static IEnumerable<int> MulCalculator(this string instructions)
+    {
+        var mulRegex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+
+        var matches = Regex.Matches(instructions, @"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)");
+        var currentOperator = string.Empty;
+
+        foreach (Match match in matches)
+        {
+            currentOperator = match.Value switch
+            {
+                "do()" => "do",
+                "don't()" => "don't",
+                _ => currentOperator
+            };
+
+            if (currentOperator == "don't" || (currentOperator is "do" && mulRegex.IsMatch(match.Value) is false))
+            {
+                continue;
+            }
+
+            var (x, y) = ParseMul(match.Value);
+            yield return x * y;
+        }
+    }
+
+    private static (int x, int y) ParseMul(string mul)
+    {
+        var values = mul.Replace("mul(", "").Replace(")", "").Split(",");
+        return (int.Parse(values[0]), int.Parse(values[1]));
+    }
+}
---
+++ 2024/AdventOfCode/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Text;
+using AdventOfCode;
+
+Console.OutputEncoding = Encoding.UTF8;
+Application.Run();
+
+
+internal abstract class Application
+{
+    private static readonly IReadOnlyDictionary<int, Action> MenuItems = new Dictionary<int, Action>
+    {
+        { 1, Day1.Run },
+        { 2, Day2.Run },
+        { 3, Day3.Run },
+    };
+
+    public static void Run()
+    {
+        Console.Clear();
+        Console.WriteLine("🎅Advent of Code 2024🎅{0}", Environment.NewLine);
+        MenuItems.ToList().ForEach(item => Console.WriteLine("{0} Day {0}", item.Key));
+        Console.WriteLine("{0} Exit", MenuItems.Count + 1);
+        Console.WriteLine("> ");
+        Console.SetCursorPosition(2, Console.CursorTop - 1);
+
+        try
+        {
+            var key = int.Parse(Console.ReadLine() ?? string.Empty);
+            if (key == MenuItems.Count + 1)
+            {
+                Console.WriteLine("Goodbye!");
+                Environment.Exit(0);
+                return;
+            }
+            Console.Clear();
+            var selected = MenuItems[key];
+            Console.WriteLine("🎅Advent of Code 2024🎅{0}Day {1}{0}", Environment.NewLine, key);
+            selected();
+            Console.ReadLine();
+            Run();
+        }
+        catch (Exception ex)
+        {
+            Console.Clear();
+            Console.WriteLine("{0}❌ {1}", Environment.NewLine, ex.Message);
+        }
+    }
+}
