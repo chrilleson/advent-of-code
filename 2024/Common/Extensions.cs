895129c4899b8adacc2eeb5f0c80765df56aa9a2
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2024-12-08 18:47:47 +0100

    Day 7 complete

--- 2024/Common/Extensions.cs
+++ 2024/Common/Extensions.cs
@@ -14,4 +14,7 @@ public static class Extensions
 
     public static IEnumerable<int> ParseInts(this string line) =>
         Regex.Matches(line, @"\d+").Select(match => int.Parse(match.Value));
+
+    public static IEnumerable<long> ParseLongs(this string line) =>
+        Regex.Matches(line, @"\d+").Select(match => long.Parse(match.Value));
 }
