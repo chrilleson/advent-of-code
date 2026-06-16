fb1383a62c441e7372da0dfd346eccfa55096d29
Christoffer Ljungqvist <christoffer.ljungqvist@protonmail.com>
2026-06-16 22:28:48 +0200

    feat: updated .inputs path

--- src/go-lang/utils/utils.go
+++ src/go-lang/utils/utils.go
@@ -8,7 +8,7 @@ import (
 
 func ReadInputFile(year string, fileName string) []byte {
 	// Running from src/go-lang/: ../../.inputs/2024/day1.txt
-	filePath := filepath.Join("..", "..", ".inputs", year, fileName+".txt")
+	filePath := filepath.Join(".inputs", year, fileName+".txt")
 	data, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
